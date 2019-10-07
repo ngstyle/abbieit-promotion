79354ca26d5e3a00364c0377203b133cdf03ba90
MohdFurkan8011 <mfs0786siwal@gmail.com>
2019-10-07 12:59:26 +0530

    Miniso Ui.

---
+++ src/app/layouts/authorized/authorized.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthorizedComponent } from './authorized.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AuthorizedComponent]
+})
+export class AuthorizedModule { }
---
+++ src/app/layouts/unauthorized/unauthorized.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UnauthorizedComponent } from './unauthorized.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [UnauthorizedComponent]
+})
+export class UnauthorizedModule { }
