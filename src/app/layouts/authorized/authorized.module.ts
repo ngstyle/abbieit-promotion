fb1210658f169427fec6b85f4da342f6d8b8d1b6
MohdFurkan8011 <mfs0786siwal@gmail.com>
2019-10-07 13:48:17 +0530

    Routing......

--- src/app/layouts/authorized/authorized.module.ts
+++ src/app/layouts/authorized/authorized.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthorizedComponent } from './authorized.component';
+import { AuthorizedRoutes } from './authorized.routing';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AuthorizedRoutes
   ],
   declarations: [AuthorizedComponent]
 })
---
+++ src/app/layouts/authorized/authorized.routing.ts
@@ -0,0 +1,30 @@
+import { AuthorizedComponent } from './authorized.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthorizedComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(mod => mod.DashboardModule)
+      },
+      {
+        path: 'marchant',
+        loadChildren: () => import('./merchant/merchant.module').then(mod => mod.MerchantModule)
+      },
+      {
+        path: 'report',
+        loadChildren: () => import('./report/report.module').then(mod => mod.ReportModule)
+      }
+    ],
+  },
+];
+
+export const AuthorizedRoutes = RouterModule.forChild(routes);
