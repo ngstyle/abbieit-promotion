8e70131b2f79be0ca5139483cf36e2a3f87a65e9
MohdFurkan8011 <mfs0786siwal@gmail.com>
2019-10-10 16:35:20 +0530

    Miniso..

--- src/app/layouts/authorized/dashboard/dashboard.component.ts
+++ src/app/layouts/authorized/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PermissionService } from 'src/app/service/permission.service';
 import { Router } from '@angular/router';
-import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,16 +9,10 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 })
 export class DashboardComponent implements OnInit {
 
-  radius = 80;
   constructor(private permissionService: PermissionService,
-              private router: Router,
-              private deviceService: DeviceDetectorService) {
+              private router: Router) {
     if (! this.permissionService.hasPermission('miniso_admin')) {
       this.router.navigate(['marchant']);
-    }
-
-    if (this.deviceService.isMobile()) {
-      this.radius = 40;
     }
   }
 
