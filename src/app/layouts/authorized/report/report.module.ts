2475af82d80fa03e1f0df4a387da2d3e99be7066
MohdFurkan8011 <mfs0786siwal@gmail.com>
2019-10-12 12:43:40 +0530

    Miniso report.

--- src/app/layouts/authorized/report/report.module.ts
+++ src/app/layouts/authorized/report/report.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReportComponent } from './report.component';
 import { Routes, RouterModule } from '@angular/router';
+import { MatTableModule, MatButtonModule, MatPaginatorModule } from '@angular/material';
 
 const routes: Routes = [
   { path: '', component: ReportComponent }
@@ -12,7 +13,10 @@ const ReportRouter = RouterModule.forChild(routes);
 @NgModule({
   imports: [
     CommonModule,
-    ReportRouter
+    ReportRouter,
+    MatTableModule,
+    MatButtonModule,
+    MatPaginatorModule
   ],
   declarations: [ReportComponent]
 })
