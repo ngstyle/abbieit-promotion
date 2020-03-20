99c6040be5d4bab5456e08eb657cde16ed2f0f81
dengchong <dengchong@ltbl.cn>
2020-03-20 22:32:15 +0800

    img display block

--- src/app/layouts/unauthorized/registration/registration.module.ts
+++ src/app/layouts/unauthorized/registration/registration.module.ts
@@ -4,14 +4,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration.component';
 import { Routes, RouterModule } from '@angular/router';
-// import { MatButtonModule, MatFormFieldModule, 
-//   MatInputModule, MatToolbarModule } from '@angular/material';
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
